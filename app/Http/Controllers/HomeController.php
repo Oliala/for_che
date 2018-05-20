ebfc50ab7fe38ab4e863ea3da6f032fe01729f79
Oliala <oliala.oo@gmail.com>
2018-05-20 19:42:44 +0300

    add avatar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\DemoMail;
+
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +28,14 @@ class HomeController extends Controller
      */
     public function index()
     {
+//        $email = Auth::user()->email;
+//        Mail::to($email)->send(new DemoMail());
         return view('home');
+    }
+
+    public function profile()
+    {
+        $user = Auth::user();
+        return view('profile',compact('user',$user));
     }
 }
